ee47b2090dc680648e5facc56785885344955ab1
shamseer8877 <alishamseer@gmail.com>
2024-04-09 13:48:48 +0530

    initial commit

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+import Navbar from "../component/Navbar";
+
+const MainLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+export default MainLayout;
